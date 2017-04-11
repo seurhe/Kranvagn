ac2dd662f2ddd8cc6bc0b1f49c5645e5e0e2db9f
Fosselius Jesper <jesper.fosselius@sweco.se>
2017-04-11 14:46:58 +0200

    Kaj går att få upp i Hololens

---
+++ Unity/Kaj/Assets/Scripts/ApDis.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApDis : MonoBehaviour {
+
+    public bool Aktiv = true;
+
+
+
+    public void ApDiss()
+    {
+        if (Aktiv)
+        {
+            gameObject.SetActive(false);
+        }
+
+        else
+        {
+            gameObject.SetActive(true);
+            Aktiv = false;
+        }
+    }
+}
